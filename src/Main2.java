280cafaad515cbd30cb70239e72585562269b59f
Juanpa Quesada <jpqcaballero@gmail.com>
2024-02-20 10:45:29 -0600

    se crea main 2 de ejemplo en la clase y se hace update del normal

---
+++ src/Main2.java
@@ -0,0 +1,7 @@
+public class Main2 {
+
+    public static void main(String[] args) {
+        Main m = new Main(); //creamos una instancia de la clase main, la cual es "m"
+        //probamos que esto esta en una jerarquia
+    }
+}
